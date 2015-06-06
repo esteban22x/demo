b7c723955b4a9b84bbe30c79fe34d0fdd22a4b00
Esteban <esteban22x@gmail.com>
2015-06-05 23:28:55 -0500

    demostracion
    descripcion

---
+++ catalogo/apps/webservices/ws_productos/serializer.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from catalogo.apps.ventas.models import Producto, Marca, Categoria
+
+class producto_serializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = Producto
+		fields = ('url','descripcion','status','nombre','imagen','precio','marca','categorias',)
+class marca_serializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = Marca
+		fields = ('url','nombre',)
+class categoria_serializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = Categoria
+		fields = ('url','nombre',)			
